b119f555f7bb6b6506714f1f4dcf160e4820588b
chris250299 <tradang.isvnu@gmail.com>
2018-12-19 19:13:43 +0700

    Point and Moveable point

---
+++ src/MoveableTest.java
@@ -0,0 +1,6 @@
+public class MoveableTest {
+    public static void main(String[] args) {
+        MoveablePoint a = new MoveablePoint(3, 4, 9, 4);
+        System.out.println(a);
+    }
+}
---
+++ src/PointTest.java
@@ -0,0 +1,8 @@
+public class PointTest {
+    public static void main(String[] args) {
+        Point p = new Point();
+        Point p1 = new Point(1,2);
+        System.out.println(p);
+        System.out.println(p1);
+    }
+}
